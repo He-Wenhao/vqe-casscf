4ae73a58b1535df1240653e65ccaa6d71d38df63
Noah Song <noahsong2006@gmail.com>
2025-08-12 17:01:01 -0400

    Update Figure_5.py

--- Figure_5.py
+++ Figure_5.py
@@ -20,96 +20,65 @@ import matplotlib as mpl
 
 from utils import *
 from vqe_driver import *
+from checker import *
 
 
-d = 2.3
-pos = [[0,0,0], [d,0,0], [2*d,0,0], [3*d,0,0]]
-elements = ["H", "H", "H", "H"]
-atom = [[el, tuple(c)] for el, c in zip(elements, pos)]
+def load_projection_data(filename="obs/20.json", atom=None):
+    obs_path = Path(filename)
+    obs_data = json.load(open(obs_path))
+    proj = np.array(obs_data["proj"])
 
-# Load projection from obs/20.json
-obs_path = Path("obs/20.json")
-obs_data = np.array(scipy.json.load(open(obs_path))) if False else None
-# actually use JSON
-obs_data = json.load(open(obs_path))
-proj = np.array(obs_data["proj"])
+    # Overlap and permutation
+    S = gto.M(atom=atom, basis="cc-pVDZ").intor("int1e_ovlp")
+    sqrtS = scipy.linalg.sqrtm(S).real
+    perm = perm_orca2pyscf(atom=atom, basis="cc-pVDZ")
+    proj = perm @ proj @ perm.T
 
-# Overlap and permutation
-S = gto.M(atom=atom, basis="cc-pVDZ").intor("int1e_ovlp")
-sqrtS = scipy.linalg.sqrtm(S).real
-perm = perm_orca2pyscf(atom=atom, basis="cc-pVDZ")
-proj = perm @ proj @ perm.T
+    # NN initial guess from projection
+    eigvals, eigvecs = eigh(proj)
+    idx = np.argsort(eigvals)[::-1]
+    sqrtS_inv = inv(sqrtS)
+    sorted_eigvecs = sqrtS_inv @ eigvecs[:, idx]
 
-# Build molecule
-mol = gto.M(atom=atom, basis='cc-pVDZ', spin=0, charge=0, verbose=0)
-ncas, nelecas = 4, 4
+    return sorted_eigvecs
 
-# NN initial guess from projection
-eigvals, eigvecs = eigh(proj)
-idx = np.argsort(eigvals)[::-1]
-sqrtS_inv = inv(sqrtS)
-sorted_eigvecs = sqrtS_inv @ eigvecs[:, idx]
 
-"""========FCI=========="""
-# HF initial guess
-mf_hf = scf.RHF(mol)
-mf_hf.kernel()
-fci_E_hf, fci_params_hf, fci_hf_orbitals, fci_hf_ci, mc_hf_ci = run_vqe_cas(mol,  mf_hf.mo_coeff, ncas, nelecas,solver='FCI')
-print(f"HF-initialized CI-CAS energy: {fci_E_hf:.8f} Ha")
+def run_fci_calculations(mol, hf_mo_coeff, nn_mo_coeff, ncas=4, nelecas=4):
+    print("=== Running FCI Calculations ===")
+    
+    # FCI with HF initial guess
+    fci_E_hf, fci_params_hf, fci_hf_orbitals, fci_hf_ci, mc_hf_ci = run_vqe_cas(
+        mol, hf_mo_coeff, ncas, nelecas, solver='FCI'
+    )
+    print(f"HF-initialized FCI-CAS energy: {fci_E_hf:.8f} Ha")
+    
+    # FCI with NN initial guess
+    fci_E_nn, fci_params_nn, fci_nn_orbitals, fci_nn_ci, mc_nn_ci = run_vqe_cas(
+        mol, nn_mo_coeff, ncas, nelecas, solver='FCI'
+    )
+    print(f"NN-initialized FCI-CAS energy: {fci_E_nn:.8f} Ha")
 
-# run classical vqe
-fci_E_nn, fci_params_nn, fci_nn_orbitals, fci_nn_ci, mc_nn_ci = run_vqe_cas(mol,  sorted_eigvecs, ncas, nelecas,solver='FCI')
-print(f"HF-initialized CI-CAS energy: {fci_E_nn:.8f} Ha")
+    return (fci_E_hf, fci_params_hf, fci_hf_orbitals, fci_hf_ci, mc_hf_ci,
+            fci_E_nn, fci_params_nn, fci_nn_orbitals, fci_nn_ci, mc_nn_ci)
 
-np.linalg.norm(fci_hf_orbitals[:,:4] @ fci_hf_orbitals[:,:4].T - fci_nn_orbitals[:,:4] @ fci_nn_orbitals[:,:4].T)
 
-"""========VQE-CASSCF=========="""
-# Run for NN guess
-E_nn, params_nn,nn_orbitals, nn_ci, mc_nn  = run_vqe_cas(mol, sorted_eigvecs, ncas, nelecas)
-print(f"NN-initialized VQE-CAS energy: {E_nn:.8f} Ha")
+def run_vqe_casscf_calculations(mol, hf_mo_coeff, nn_mo_coeff, ncas=4, nelecas=4):
+    print("\n=== Running VQE-CASSCF Calculations ===")
+    
+    # VQE with NN initial guess
+    E_nn, params_nn, nn_orbitals, nn_ci, mc_nn = run_vqe_cas(
+        mol, nn_mo_coeff, ncas, nelecas
+    )
+    print(f"NN-initialized VQE-CAS energy: {E_nn:.8f} Ha")
 
-# HF initial guess
-mf_hf = scf.RHF(mol)
-mf_hf.kernel()
-E_hf, params_hf, hf_orbitals, hf_ci, mc_hf = run_vqe_cas(mol,  mf_hf.mo_coeff, ncas, nelecas)
-print(f"HF-initialized VQE-CAS energy: {E_hf:.8f} Ha")
+    # VQE with HF initial guess
+    E_hf, params_hf, hf_orbitals, hf_ci, mc_hf = run_vqe_cas(
+        mol, hf_mo_coeff, ncas, nelecas
+    )
+    print(f"HF-initialized VQE-CAS energy: {E_hf:.8f} Ha")
 
-np.linalg.norm(hf_orbitals[:,:4] @ hf_orbitals[:,:4].T - nn_orbitals[:,:4] @ nn_orbitals[:,:4].T)
+    return (E_hf, params_hf, hf_orbitals, hf_ci, mc_hf, E_nn, params_nn, nn_orbitals, nn_ci, mc_nn)
 
-"""==============================""""
-# Compute 1-RDM in the active space
-rdm1 = mc_hf_ci.fcisolver.make_rdm1(fci_hf_ci, mc_hf_ci.ncas, mc_hf_ci.nelecas)
-
-# Verify trace equals electron number (active electrons)
-trace_rdm1 = rdm1.trace()
-eigvals = np.linalg.eigvalsh(rdm1)[::-1]  # Hermitian, so eigvalsh is faster
-print("Eigenvalues of the active-space 1-RDM:")
-print(eigvals)
-
-# Compute 1-RDM in the active space
-rdm1 = mc_nn_ci.fcisolver.make_rdm1(fci_nn_ci, mc_nn_ci.ncas, mc_nn_ci.nelecas)
-
-# Verify trace equals electron number (active electrons)
-trace_rdm1 = rdm1.trace()
-eigvals = np.linalg.eigvalsh(rdm1)  # Hermitian, so eigvalsh is faster
-print("Eigenvalues of the active-space 1-RDM:")
-print(eigvals)
-
-print((nn_ci).shape)
-print("Infid(nn-vqe,hf-vqe):", casscf_overlap(mc_hf, mc_nn))
-print("Infid(nn-vqe,fci):", casscf_overlap(mc_nn, mc_hf_ci))
-print("Infid(hf-vqe,fci):", casscf_overlap(mc_hf, mc_hf_ci))
-print("Infid(fci-1,fci-2):", casscf_overlap(mc_nn_ci, mc_hf_ci))
-
-dm1_ao_nn, dm1_ao_hf, dm2_ao_nn, dm2_ao_hf = compare_rdms_ao(mc_nn, mc_hf)
-e_nn = energy_from_ao_rdm(mol, dm1_ao_nn, dm2_ao_nn)
-e_hf = energy_from_ao_rdm(mol, dm1_ao_hf, dm2_ao_hf)
-
-print(f"Energy from NN-init RDMs: {e_nn:.12f} Ha")
-print(f"Energy from HF-init RDMs: {e_hf:.12f} Ha")
-print(f"ΔE = {e_nn - e_hf:.6e} Ha")
-
-"""========Figure 5 Scanning Function========"""
 # Main scanning function
 def extended_vqe_optimization_scan(mol, mo1, mo2, params1, params2, ncas, nelecas, t_min=-0.3, t_max=1.3, n_points=21):
     S = mol.intor("int1e_ovlp")
@@ -141,16 +110,6 @@ def extended_vqe_optimization_scan(mol, mo1, mo2, params1, params2, ncas, neleca
     return ts, np.array(energies_opt), params_opt, converged
 
 
-# Run scan from -0.2 to 1.2
-ts_ext, E_opt_ext, params_opt_ext, converged_ext = extended_vqe_optimization_scan(
-    mol, hf_orbitals, nn_orbitals, params_hf, params_nn,
-    ncas, nelecas, t_min=-0.2, t_max=1.2, n_points=29
-)
-
-"""=============Save Data & Plot Sample Figure============="""
-mpl.rcParams['pdf.fonttype'] = 42
-
-# Visualization function
 def visualize_extended_scan(ts, energies, converged, E_hf_orig, E_nn_orig):
     energies = np.array(energies)
     converged = np.array(converged)
@@ -171,7 +130,9 @@ def visualize_extended_scan(ts, energies, converged, E_hf_orig, E_nn_orig):
     
     print("Warning: set E_ref manually")
     E_ref = -2.0140511195198982
-    rel = (energies - E_ref) 
+    rel = (energies - E_ref)
+
+    mpl.rcParams['pdf.fonttype'] = 42
 
     # plt.figure(figsize=(10, 8))
     plt.figure()
@@ -196,15 +157,50 @@ def visualize_extended_scan(ts, energies, converged, E_hf_orig, E_nn_orig):
     
     return idx_min, idx_max
 
-# Visualize results
-idx_min, idx_max = visualize_extended_scan(ts_ext, E_opt_ext, converged_ext, E_hf, E_nn)
+def main():
+    mol, atom = setup_molecule(d=2.3)
+    ncas, nelecas = 4, 4
 
-# Save results for further analysis
-results = {
-    't': ts_ext,
-    'energy': E_opt_ext,
-    'converged': converged_ext,
-    'params': params_opt_ext,
-    'E_hf_original': E_hf,
-    'E_nn_original': E_nn
-}
+    nn_mo_coeff = load_projection_data("obs/20.json", atom)
+
+    mf_hf = scf.RHF(mol)
+    mf_hf.kernel()
+    hf_mo_coeff = mf_hf.mo_coeff
+
+    # Run FCI calculations
+    (fci_E_hf, fci_params_hf, fci_hf_orbitals, fci_hf_ci, mc_hf_ci, fci_E_nn, fci_params_nn, fci_nn_orbitals, fci_nn_ci, mc_nn_ci) = run_fci_calculations(
+        mol, hf_mo_coeff, nn_mo_coeff, ncas, nelecas)
+    
+    # Run VQE calculations  
+    (E_hf, params_hf, hf_orbitals, hf_ci, mc_hf, E_nn, params_nn, nn_orbitals, nn_ci, mc_nn) = run_vqe_casscf_calculations(
+        mol, hf_mo_coeff, nn_mo_coeff, ncas, nelecas)
+
+    # Sanity checks
+    analyze_rdms(mc_hf_ci, fci_hf_ci, mc_nn_ci, fci_nn_ci, ncas, nelecas)
+    infidelities_and_energies(mc_hf, mc_nn, mc_hf_ci, mc_nn_ci, mol)
+    orbital_difference(hf_orbitals, nn_orbitals, fci_hf_orbitals, fci_nn_orbitals)
+
+    # Run extended VQE optimization scan
+    ts_ext, E_opt_ext, params_opt_ext, converged_ext = extended_vqe_optimization_scan(
+        mol, hf_orbitals, nn_orbitals, params_hf, params_nn,
+        ncas, nelecas, t_min=-0.2, t_max=1.2, n_points=29
+    )
+
+    # Visualize and analyze results
+    idx_min, idx_max = visualize_extended_scan(ts_ext, E_opt_ext, converged_ext, E_hf, E_nn)
+
+    # Save results for further analysis
+    results = {
+        't': ts_ext.tolist(),
+        'energy': E_opt_ext.tolist(),
+        'converged': converged_ext,
+        'params': params_opt_ext,
+        'E_hf_original': float(E_hf),
+        'E_nn_original': float(E_nn)
+    }
+
+    return results
+
+
+if __name__ == "__main__":
+    results = main()
