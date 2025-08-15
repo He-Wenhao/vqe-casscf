131d89eae8dd16cbf9a8c16d86cea4028e3df9ae
Noah Song <noahsong2006@gmail.com>
2025-08-15 14:31:15 -0400

    Update Figure_3.py

--- Figure_3.py
+++ Figure_3.py
@@ -47,6 +47,7 @@ def pennylane_vqe_casci(H_const, h1, g2, nele, init_params=None, track_history=T
     qub_ham = jordan_wigner(ferm_ham)
     n_qubits = max(i for term in qub_ham.terms for i, _ in term) + 1
     Hmat = get_sparse_operator(qub_ham, n_qubits).toarray()
+    Hmat = (Hmat + Hmat.conj().T) / 2
     H = qml.Hermitian(Hmat, wires=range(n_qubits))
 
     # UCCSD ansatz
